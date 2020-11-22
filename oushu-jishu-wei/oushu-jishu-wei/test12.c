54afb48ab8a25dd457a4cd930cbfcf637c637f58
DavenportChen <1163055357@qq.com>
2020-11-22 16:17:21 +0800

    第二次调试

---
+++ oushu-jishu-wei/oushu-jishu-wei/test12.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+void Printbit(int num)
+{
+	for (int i = 31; i >= 1; i -= 2)
+	{
+		printf("%d ", (num >> i) & 1);
+	}
+	printf("\n");
+	for (int i = 30; i >= 0; i -= 2)
+	{
+		printf("%d ", (num >> i) & 1);
+	}
+	printf("\n");
+}
+int main()
+{
+	int num = 0;
+	printf("请输入一个数： ");
+	scanf("%d", &num);
+	Printbit(num);
+	return 0;
+}
